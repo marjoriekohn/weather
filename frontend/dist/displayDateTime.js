8c042c6152d76aa1cbc0081b02792ce0061c02c1
Marjorie Kohn <marjoriekohn01@gmail.com>
2023-10-10 18:17:21 -0400

    add moment ref path and script

--- frontend/dist/displayDateTime.js
+++ frontend/dist/displayDateTime.js
@@ -1,5 +1,5 @@
 "use strict";
-const moment = require('moment');
+///<reference path="../../node_modules/moment/ts3.1-typings/moment.d.ts"/>
 const currentTime = moment().format('MMMM Do YYYY, h:mm a');
 (document.getElementById('current-time')).innerHTML = currentTime;
 //# sourceMappingURL=displayDateTime.js.map
--- frontend/ts/displayDateTime.ts
+++ frontend/ts/displayDateTime.ts
@@ -1,4 +1,3 @@
-const moment = require('moment');
-
+///<reference path="../../node_modules/moment/ts3.1-typings/moment.d.ts"/>
 const currentTime = moment().format('MMMM Do YYYY, h:mm a');
 (document.getElementById('current-time')!).innerHTML = currentTime;
